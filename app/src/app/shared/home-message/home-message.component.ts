2f9f055ee4f797bd8ecebf86bbf2509444dbfd8d
Milen961 <theheller178@gmail.com>
2022-12-07 13:09:45 +0200

    Add a shared componnets

---
+++ app/src/app/shared/home-message/home-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-message',
+  templateUrl: './home-message.component.html',
+  styleUrls: ['./home-message.component.scss']
+})
+export class HomeMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
