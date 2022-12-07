8a6883c7d38d702f38150d9af937c627cc187d1c
Milen961 <theheller178@gmail.com>
2022-12-07 12:55:20 +0200

    Add a auth componets for user

---
+++ app/src/app/auth-controller/auth-controller.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthControllerModule { }
