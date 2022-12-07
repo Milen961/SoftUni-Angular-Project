2657e95219b6593f7dedc7bcf69c88bc72f7f4c1
Milen961 <theheller178@gmail.com>
2022-12-07 12:50:31 +0200

    Add a core components

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,41 +1,41 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './core/error/error.component';
-import { HomeComponent } from './core/home/home.component';
-import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+// import { ErrorHandler, NgModule } from '@angular/core';
+// import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './core/home/home.component';
+// import { Page404Component } from './core/page404/page404.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent
-  },
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent
-  },
-  {
-    path: 'error',
-    component: ErrorComponent
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'theme',
-    loadChildren: () => import('./theme/theme.module').then(m => m.ThemeModule)
-  },
-  {
-    path: '**',
-    redirectTo: '/not-found'
-  }
-];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {
+// const routes: Routes = [
+//   {
+//     path: '',
+//     pathMatch: 'full',
+//     component: HomeComponent
+//   },
+//   {
+//     path: 'not-found',
+//     component: Page404Component
+//   },
+//   {
+//     path: 'error',
+//     component: ErrorHandler
+//   },
+//   {
+//     path: 'auth',
+//     loadChildren: () => import('./authController/authController.module').then(m => m.AuthModule)
+//   },
+//   {
+//     path: 'theme',
+//     loadChildren: () => import('./theme/theme.module').then(m => m.ThemeModule)
+//   },
+//   {
+//     path: '**',
+//     redirectTo: '/not-found'
+//   }
+// ];
 
-}
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {
+
+// }
--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ErrorHandlerComponent } from './error-handler/error-handler.component';
+import { Page404Component } from './page404/page404.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ErrorHandlerComponent,
+    Page404Component
   ],
   imports: [
     CommonModule
