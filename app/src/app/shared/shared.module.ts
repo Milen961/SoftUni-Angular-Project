ba52defc5a39841fc3053e00f5ebe19f0c72d81a
Milen961 <theheller178@gmail.com>
2022-12-08 19:55:09 +0200

    Update sharedmodule.ts

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { HomeMessageComponent } from './home-message/home-message.component';
+import { EmailDirective } from './emailValidator';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    HomeMessageComponent
+    HomeMessageComponent,
+    EmailDirective
+    
+    
   ],
   imports: [
     CommonModule
+  ],
+
+  exports:[
+    LoaderComponent,
+    HomeMessageComponent,
+    EmailDirective,
+
   ]
+   
 })
 export class SharedModule { }
