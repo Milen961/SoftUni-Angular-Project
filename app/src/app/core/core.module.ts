291c65ebab8b59965b2f8fee7eee4a5f97dd07cd
Milen961 <theheller178@gmail.com>
2022-12-08 17:46:26 +0200

    Update core module

--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorHandlerComponent } from './error-handler/error-handler.component';
 import { Page404Component } from './page404/page404.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -17,7 +18,16 @@ import { Page404Component } from './page404/page404.component';
     Page404Component
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+
+  ],
+  exports: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    Page404Component,
+    ErrorHandlerComponent
   ]
 })
 export class CoreModule { }
