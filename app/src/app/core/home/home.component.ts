e23d0f97cbb1d106a881a94c0de730202dcb4963
Milen961 <theheller178@gmail.com>
2022-12-09 21:59:40 +0200

    Add a homecomponent

--- app/src/app/core/home/home.component.ts
+++ app/src/app/core/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor() { }
+    get loggedIn(){
+     return this.authService.loggedIn
+    }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
