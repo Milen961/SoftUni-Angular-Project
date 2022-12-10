696b015f3fb6e58196103bfb051b034a7415f615
Milen961 <theheller178@gmail.com>
2022-12-10 17:16:26 +0200

    separated

--- app/src/app/shared/emailValidator/email-validation.ts
+++ app/src/app/shared/emailValidator/email-validation.ts
@@ -1,20 +1,11 @@
- import { ValidatorFn, FormGroup} from "@angular/forms";
+ import { ValidatorFn } from "@angular/forms";
 
  export function emailValidation(domains: String[]): ValidatorFn {
 
- const stringDomain = domains.join('|');
- const reGex = new RegExp(`^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}
-    [a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$\.(${stringDomain})`);
+ const stringDomain = domains.join('|');  //^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$
+ const reGex = new RegExp(`^[^@]{6,}@gmail\.(${stringDomain})$`);
     return (con) => {
        return(con.value === '' || reGex.test(con.value)) ? null : { emailValidation: true }
     };
  }
 
- export function sameValueEmails(em1: string, em2: string): ValidatorFn {
-    return (control) => {
-      const group = control as FormGroup;
-      const ctrl1 = group.get(em1);
-      const ctrl2 = group.get(em2)
-      return ctrl1?.value === ctrl2?.value ? null : { sameValueEmails: true };
-    };
-  }
