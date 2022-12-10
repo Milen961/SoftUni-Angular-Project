dcadec9904d9ccee4871bed3062a6f0ecb665621
Milen961 <theheller178@gmail.com>
2022-12-10 20:39:50 +0200

    Add a unfinished register component

--- app/src/app/auth/register/register.component.ts
+++ app/src/app/auth/register/register.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { catchError, map, of, throwError } from 'rxjs';
+import { EmailVerificator } from 'src/app/shared/verifications';
+import { emailValidation } from 'src/app/shared/emailValidator';
+import { sameValueGroup } from 'src/app/shared/emailValidator/same-value-group';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidation(EmailVerificator)]],
+    pass: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: []
+    }, {
+      validators: [sameValueGroup('password', 'rePassword')]
+    })
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+
+  registerHandler() {
+    if (this.form.invalid) 
+    { return; }
+    const { username, email, pass: { password, rePassword } = {} } = this.form.value;
+    this.authService.register(username!, email!, password!, rePassword!)
+      .subscribe(user => {
+        this.router.navigate(['/book/recent']);
+      });
   }
-
 }
