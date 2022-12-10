17ff3d657ccbd5f12149b122ddd74aa0e1173aa4
Milen961 <theheller178@gmail.com>
2022-12-10 18:51:42 +0200

    Add a error-handler

--- app/src/app/core/error-handler/error-handler.component.ts
+++ app/src/app/core/error-handler/error-handler.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, debounceTime, filter, take } from 'rxjs';
+import { apiError } from 'src/app/shared/verifications';
+
 
 @Component({
   selector: 'app-error-handler',
   templateUrl: './error-handler.component.html',
   styleUrls: ['./error-handler.component.scss']
 })
-export class ErrorHandlerComponent implements OnInit {
+export class ErrorHandlerComponent implements OnDestroy {
 
-  constructor() { }
+  apiError$ = this.apiError.asObservable();
 
-  ngOnInit(): void {
+  constructor(
+    @Inject(apiError) private apiError: BehaviorSubject<Error | null>,
+    private router: Router
+  ) {
+    this.apiError$.pipe(debounceTime(0), take(1), filter(val => !val)).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.apiError.next(null);
   }
 
 }
+
