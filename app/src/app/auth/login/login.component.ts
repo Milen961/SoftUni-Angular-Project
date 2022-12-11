25c5074174c9aacd46377af805e9d779accc41fc
Milen961 <theheller178@gmail.com>
2022-12-11 19:26:26 +0200

    Add a working login

--- app/src/app/auth/login/login.component.ts
+++ app/src/app/auth/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
+import { FormBuilder } from '@angular/forms';
+import { emailValidation } from 'src/app/shared/emailValidator';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  form = this.fb.group({
+    email: ['', [Validators.required, emailValidation()]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  })
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
+
   }
 
+  loginHandler(){
+    if (this.form.invalid) { return; } 
+    const { email, password } = this.form.value;
+    this.authService.login(email!, password!)
+      .subscribe(user => {
+        this.router.navigate(['/book/recent']);
+      });
+
+    
+  }
 }
