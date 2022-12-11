063f8988007a94b309a57c987d5e2d02245ecef0
Milen961 <theheller178@gmail.com>
2022-12-11 19:27:02 +0200

    Add a working logout

--- app/src/app/auth/logout/logout.component.ts
+++ app/src/app/auth/logout/logout.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent  {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+     this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/'])
+      },
+      error: () => {
+        this.router.navigate(['/'])
+      }
 
-  ngOnInit(): void {
-  }
+    })
+   }
+
 
 }
