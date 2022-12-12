e1a2483eb9c4d544943700abe60c549cfe8e152e
Milen961 <theheller178@gmail.com>
2022-12-12 14:16:56 +0200

    Add service functions for the books

---
+++ app/src/app/book/book.service.ts
@@ -0,0 +1,36 @@
+import { IBook } from '../shared/interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  getBook(id: string) {
+    return this.http.get<IBook>('/api/books/' + id);
+  }
+
+  getBooks(maxCount?: number) {
+    let url = '/api/books';
+    if (maxCount) {
+      url += '?limit=5';
+    }
+    return this.http.get<IBook[]>(url);
+  }
+
+
+  createBook(name: string, text: string) {
+    return this.http.post<IBook>('/api/books/', { bookName: name, postText: text });
+  }
+
+  updateBook(id: string, name: string, text: string) {
+    return this.http.put<IBook>('/api/books/' + id, { bookName: name, postText: text });
+  }
+
+  deleteBook(bookId: string, postId: string) {
+    return this.http.delete<IBook>('/api/books/' + bookId + '/post' + postId);
+  }
+}
