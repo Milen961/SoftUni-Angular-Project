9e1c2745f7cf57e764378c16eb259e2b8ff6f2ce
Milen961 <theheller178@gmail.com>
2022-12-12 14:11:04 +0200

    Uddate book.module

--- app/src/app/book/book.module.ts
+++ app/src/app/book/book.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { BookRoutingModule } from './book-routing.module';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    CreateBookComponent,
+    BookListComponent,
+    MainComponent,
+    BookDetailComponent,
+    RecentPostsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    BookRoutingModule
+
+  ],
+  exports: [
+    BookListComponent
   ]
 })
 export class BookModule { }
