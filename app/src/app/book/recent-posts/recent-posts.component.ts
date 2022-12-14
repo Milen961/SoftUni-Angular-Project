016979640543c46860c3f796e7b9ffb1b4060419
Milen961 <theheller178@gmail.com>
2022-12-14 20:09:56 +0200

    Add unfinished recent posts

---
+++ app/src/app/book/recent-posts/recent-posts.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { IPost } from 'src/app/shared/interfaces';
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrls: ['./recent-posts.component.scss']
+})
+export class RecentPostsComponent implements OnInit {
+
+  posts: IPost[] | null = null;
+   errorData = false ;
+  
+
+  constructor(private apiService: ApiService) {  }
+
+  ngOnInit(): void {
+    this.apiService.loadPosts(5).subscribe({
+      next: (value) => {
+        this.posts = value;
+
+      },
+      error: (error) => {
+        this.errorData = true;
+        console.log(error)
+      }
+    });
+  }
+
+}
