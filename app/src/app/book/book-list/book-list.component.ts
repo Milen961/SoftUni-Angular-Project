c4126b2bb8ee79dcc8bb9b2e5fb42f7e7665b8d8
Milen961 <theheller178@gmail.com>
2022-12-14 15:58:19 +0200

    Add unfinesh book-list

--- app/src/app/book/book-list/book-list.component.ts
+++ app/src/app/book/book-list/book-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { IBook } from 'src/app/shared/interfaces';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  constructor() { }
+  bookList : IBook[] | null = null;
+  errorData = false
+
+  constructor(private apiService: ApiService, private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.apiService.loadBooks().subscribe({
+      next: (value) => {
+        this.bookList = value;
+      },
+      error: (error) => {
+        this.errorData = true;
+        console.log(error);
+      }
+    });
   }
+  
 
 }
